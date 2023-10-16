d76958a27ba8b9f72671c883e0bd97e6ed2318ef
Kniggerich <domkoeln@hotmail.de>
2023-10-16 13:59:03 +0200

    changed ai submission to allow streaminig

--- src/hooks/useOpenAiSubmission.js
+++ src/hooks/useOpenAiSubmission.js
@@ -18,28 +18,45 @@ const useOpenAiSubmission = () => {
         setLoading(true);
         setError(null);
 
+        const apiUrl = `/api/openai/request?inputType=${openAiInputType}`;
+
         try {
-            const apiUrl = `/api/openai/request?inputType=${openAiInputType}`;
+            if (stream) {
+                // Streaming scenario
+                const eventSource = new EventSource(`${apiUrl}&stream=true`);
 
-            const response = await axios.post(apiUrl, {
-                promptId,
-                userMessage: openAiInput,
-                model: model
-            });
+                eventSource.onmessage = (event) => {
+                    const message = JSON.parse(event.data);
+                    // Do something with each streamed message, e.g., append it to some state variable
+                    setElevenLabsInput(prev => prev + message);
+                };
 
-            if (!response || !response.data) throw new Error ( "No response")
+                eventSource.onerror = (error) => {
+                    eventSource.close();
+                    setError(<div style={{whiteSpace: "pre-line"}}><Text>{`Streaming error: ${error.message}`}</Text></div>);
+                    setLoading(false);
+                };
 
-            setElevenLabsInput(response.data.data.message);
-            setOpenAiCallId(response.data.openAiCallId);
+            } else {
+                // Non-streaming scenario
+                const response = await axios.post(apiUrl, {
+                    promptId,
+                    userMessage: openAiInput,
+                    model: model
+                });
+
+                if (!response || !response.data) throw new Error("No response");
+
+                setElevenLabsInput(response.data.data.message);
+                setOpenAiCallId(response.data.openAiCallId);
+            }
 
         } catch (err) {
-            console.log(err.response.data.error)
-            setError(
-                <div style={{whiteSpace: "pre-line"}}><Text>{`An error occurred while processing your request: \n ${err.response.data.error}`}</Text></div>
-            );
+            console.log(err.response.data.error);
+            setError(<div style={{whiteSpace: "pre-line"}}><Text>{`An error occurred while processing your request: \n ${err.response.data.error}`}</Text></div>);
 
         } finally {
-            setLoading(false);
+            if (!stream) setLoading(false);
         }
     };
 
